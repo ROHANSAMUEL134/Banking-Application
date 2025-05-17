60318d1baa59720b86d7d011bcadc755268196e8
ROHAN SAMUEL J <rohansamuel134@gmail.com>
2025-05-17 21:44:25 +0530

    Update Transaction.java

--- Transaction.java
+++ Transaction.java
@@ -1,15 +1,15 @@
 
 import java.time.LocalDateTime;
 import java.util.Scanner;
-public class  Transaction{
+public class Transaction{
     public static void history(){
         @SuppressWarnings("resource")
         Scanner scanner = new Scanner(System.in);
         try  {
             
-                System.out.println("enter account no:");
+                System.out.println("Enter account no:");
                 int accNo=Integer.parseInt(scanner.nextLine());
-                System.out.println("enter password");
+                System.out.println("Enter password:");
                 String password=scanner.nextLine();
                 Customer temp=null;
                 for(Customer cust:Create.customers){
@@ -20,7 +20,7 @@ public class  Transaction{
                     }
                 }
                 if(temp==null){
-                    System.out.println("your account not exists");
+                    System.out.println("Wrong AccNo or password!!");
                 }
                 else{
                     FileHandler.readTransactionHistory(temp.getAccNo());
@@ -32,7 +32,7 @@ public class  Transaction{
         }
         catch(NumberFormatException e)
         {
-            System.out.println("innvalid account number");
+            System.out.println("Invalid account number");
         }
         catch(Exception e){
             System.out.println(e.getMessage());
@@ -44,14 +44,14 @@ public class  Transaction{
         @SuppressWarnings("resource")
         Scanner scanner = new Scanner(System.in);
         try{
-            System.out.println("enter the your (sender) account no");
+            System.out.println("Enter your (Sender) account no");
             int senderAccNo=Integer.parseInt(scanner.nextLine());
-            System.out.println("enter your(Sender) password");
+            System.out.println("Enter your (Sender) password");
             String senderPassword=scanner.nextLine();
-            System.out.println("enter receiver account no");
+            System.out.println("Enter receiver account no");
             int receiverAccNo=Integer.parseInt(scanner.nextLine());
             
-            System.out.println("enter Amount");
+            System.out.println("Enter Amount");
             int amount=Integer.parseInt(scanner.nextLine());
             Customer senderObj=null;
             Customer receiverObj=null;
@@ -65,21 +65,21 @@ public class  Transaction{
                 }
             }
             if(senderObj==null && receiverObj==null){
-                System.out.println("sender and receiver not exists.. enter valid details");
+                System.out.println("Sender and Receiver not exists.. enter valid details");
             }
             else if(senderObj==null){
-                System.out.println("sender not exists");
+                System.out.println("Sender not exists");
             }
             else if(receiverObj==null){
-                System.out.println("receiver accno not exists");
+                System.out.println("Receiver not exists");
             }
             else if(senderObj.viewbalance()<amount){
-                System.out.println("you don't have a sufficient amount");
+                System.out.println("You don't have sufficient amount");
             }
             else{
                 senderObj.reduceAmount(amount);
                 receiverObj.addAmount(amount);
-                System.out.println("transaction success");
+                System.out.println("Transaction success");
                 // After successful transaction
                 String transactionDetails = "Sender: " + senderObj.getName() +
                 ", Receiver: " + receiverObj.getName() +
@@ -97,7 +97,7 @@ public class  Transaction{
             }
         }
         catch(NumberFormatException e){
-            System.out.println("invalid accno and amount .. check the details");
+            System.out.println("Invalid accno or amount .. check the details");
 
         }
         catch(Exception e){
