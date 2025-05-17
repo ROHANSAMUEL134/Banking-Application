0dd38b80fb9678783bb373b02620779afc3c486d
ROHAN SAMUEL J <rohansamuel134@gmail.com>
2025-05-17 21:05:10 +0530

    Update Validation.java

--- Validation.java
+++ Validation.java
@@ -1,34 +1,34 @@
 public class Validation {
     public static Boolean  nameCheck(String name){
-        if(name.length()<=2){
-            System.out.println("your name contains greater than three letters");
+        if(name.length()<=3){
+            System.out.println("Your name should contain greater than three letters");
             return false;
         }
         else if(name.matches(".*\\d.*")){
-            System.out.println("your name  contains digits");
+            System.out.println("Your name contains digits");
             return false;
         }
         else if(name.matches(".*[!@#$%^&*()-_+=}{];:''.,}].*")){
-            System.out.println("your name contains special characters");
+            System.out.println("Your name contains special characters");
             return false;
         }
         return true;
     }
     public static Boolean passwordCheck(String password){
         if(password.length()<8){
-            System.out.println("your password atleast contains 8 characters");
+            System.out.println("Your password should contain aleast 8 characters");
             return false;
         }
         else if(!password.matches(".*[A-Z].*")){
-            System.out.println("password contains atleast one uppercase letters");
+            System.out.println("Password should contain atleast one uppercase letter");
             return false;
         }
         else if(!password.matches(".*[a-z].*")){
-            System.out.println("password contains atleast one lowercase letters");
+            System.out.println("Password should contain atleast one lowercase letter");
             return false;
         }
         else if(!password.matches(".*\\d.*")){
-            System.out.println("password contains atleast one digits");
+            System.out.println("Password should contain atleast one digit");
             return false;
         }
         return true;
