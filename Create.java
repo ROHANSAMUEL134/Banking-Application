edf0cde1748591e9c8d7c60ed1ab5128a322605f
ROHAN SAMUEL J <rohansamuel134@gmail.com>
2025-05-18 10:27:49 +0530

    Update Create.java

--- Create.java
+++ Create.java
@@ -1,46 +1,60 @@
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Create{
-    static  ArrayList<Customer> customers=new ArrayList<>();
-    static HashMap<Integer,Customer> allCustomer=new HashMap<>();
-    public static String encryption(String password){
-        int n=password.length();
-        String encryptString="";
+public class Create {
+    static ArrayList<Customer> customers = new ArrayList<>();
+    static HashMap<Integer, Customer> allCustomer = new HashMap<>();
 
-        int key=3;
-        for(int i=0;i<n;i++){
-            if(Character.isDigit(password.charAt(i))){
-                encryptString+=(char)(((password.charAt(i)-'0'+key)%10)+'0');
-            }
-            else if(Character.isUpperCase(password.charAt(i))){
-                encryptString+=(char)(((password.charAt(i)-'A'+key)%26)+'A');
-            }
-            else if(Character.isLowerCase(password.charAt(i))){
+    public static String encryption(String password) {
+        int n = password.length();
+        String encryptString = "";
+        int key = 3;
 
-                encryptString+=(char)(((password.charAt(i)-'a'+key)%26)+'a');
+        for (int i = 0; i < n; i++) {
+            char ch = password.charAt(i);
+            if (Character.isDigit(ch)) {
+                encryptString += (char) (((ch - '0' + key) % 10) + '0');
+            } else if (Character.isUpperCase(ch)) {
+                encryptString += (char) (((ch - 'A' + key) % 26) + 'A');
+            } else if (Character.isLowerCase(ch)) {
+                encryptString += (char) (((ch - 'a' + key) % 26) + 'a');
+            } else {
+                encryptString += ch;
             }
-            else{
-                encryptString+=password.charAt(i);
-            }
-            
         }
         return encryptString;
     }
 
-    public static void create(String name,String password,int accNo){
-        Customer obj=new Customer();
-        obj.setAccNo(accNo);
-        obj.setName(name);
-        String encryptString=encryption(password);
-        obj.setPassword(encryptString);
-        customers.add(obj);
-        
-        allCustomer.put(accNo,obj);
+    public static void create(String name, String password, int accNo) {
+        try {
+            String encryptString = encryption(password);
+            
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3030/bank_app", "rohan", "Rohan@134");
+            String sql = "INSERT INTO customers (accNo, name, password) VALUES (?, ?, ?)";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setInt(1, accNo);
+            stmt.setString(2, name);
+            stmt.setString(3, encryptString);
+            stmt.executeUpdate();
+            con.close();
 
-        System.out.println("Account Created Successfully");
-        System.out.println("account no"+accNo);
-        System.out.println("name:"+name);
+            Customer obj = new Customer();
+            obj.setAccNo(accNo);
+            obj.setName(name);
+            obj.setPassword(encryptString);
+            customers.add(obj);
+            allCustomer.put(accNo, obj);
+
+            System.out.println("Account Created Successfully");
+            System.out.println("Account No: " + accNo);
+            System.out.println("Name: " + name);
+
+        } catch (Exception e) {
+            System.out.println("Error creating account.");
+            e.printStackTrace();
+        }
     }
 }
